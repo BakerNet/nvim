c943355248db23ff2b4c8949c4606ea84402cb9f
BakerNet <hansbaker90@gmail.com>
2023-05-29 12:44:01 -0700

    fix removing custom

--- lua/hans/lazy.lua
+++ lua/hans/lazy.lua
@@ -185,10 +185,4 @@ require('lazy').setup({
   require 'kickstart.plugins.autoformat',
   -- require 'kickstart.plugins.debug',
 
-  -- NOTE: The import below automatically adds your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-  --    up-to-date with whatever is in the kickstart repo.
-  --
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  { import = 'custom.plugins' },
 }, {})
