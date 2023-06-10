1374abc6335d9d2a0e2d5be745ad4573e9666ef2
BakerNet <hansbaker90@gmail.com>
2023-06-09 22:07:30 -0700

    dynamic veritcal terminal width

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,6 +1,12 @@
 require('toggleterm').setup({
   open_mapping = [[<C-t>]],
-  size = 20,
+  size = function(term)
+    if term.direction == "horizontal" then
+      return 20
+    elseif term.direction == "vertical" then
+      return vim.o.columns * 0.4
+    end
+  end,
   persist_mode = true
 })
 
@@ -15,5 +21,5 @@ end
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
-vim.keymap.set('n', '<leader>tv', [[<Cmd>ToggleTerm size=100 direction=vertical persist_mode=true<CR>]])
-vim.keymap.set('n', '<leader>th', [[<Cmd>ToggleTerm size=20 direction=horizontal persist_mode=true<CR>]])
+vim.keymap.set('n', '<leader>tv', [[<Cmd>ToggleTerm direction=vertical persist_mode=true<CR>]])
+vim.keymap.set('n', '<leader>th', [[<Cmd>ToggleTerm direction=horizontal persist_mode=true<CR>]])
