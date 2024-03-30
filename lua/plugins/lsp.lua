5e209c20983bf27d78757b41e6bfa4bc4d1f87e0
BakerNet <hans@multimediallc.com>
2024-03-29 18:19:05 -0700

    misc

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -155,31 +155,31 @@ local setup_lsp = function()
                 telemetry = { enable = false },
             },
         },
-        pylsp = {
-            pylsp = {
-                plugins = {
-                    -- formatter options
-                    black = { enabled = false },
-                    autopep8 = { enabled = false },
-                    yapf = { enabled = false },
-                    -- linter options
-                    ruff = { enabled = true },
-                    pylint = { enabled = false, executable = "pylint" },
-                    pyflakes = { enabled = false },
-                    pycodestyle = { enabled = false },
-                    mccabe = { enabled = false },
-                    flake8 = { enabled = false },
-                    -- type checker
-                    pylsp_mypy = { enabled = false },
-                    -- auto-completion options
-                    jedi_completion = { fuzzy = true },
-                    -- import sorting
-                    isort = { enabled = true },
-                    -- code actions
-                    rope_autoimport = { enabled = true },
-                },
-            },
-        },
+        -- pylsp = {
+        --     pylsp = {
+        --         plugins = {
+        --             -- formatter options
+        --             black = { enabled = false },
+        --             autopep8 = { enabled = false },
+        --             yapf = { enabled = false },
+        --             -- linter options
+        --             ruff = { enabled = true },
+        --             pylint = { enabled = false, executable = "pylint" },
+        --             pyflakes = { enabled = false },
+        --             pycodestyle = { enabled = false },
+        --             mccabe = { enabled = false },
+        --             flake8 = { enabled = false },
+        --             -- type checker
+        --             pylsp_mypy = { enabled = false },
+        --             -- auto-completion options
+        --             jedi_completion = { fuzzy = true },
+        --             -- import sorting
+        --             isort = { enabled = true },
+        --             -- code actions
+        --             rope_autoimport = { enabled = true },
+        --         },
+        --     },
+        -- },
     }
 
     -- Setup neovim lua configuration
